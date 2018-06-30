82bb6b25ee4935405c4bf1cb2f6a380942f2f459
michael rhyndress <michaelrhyndress@gmail.com>
2018-06-30 15:57:16 -0400

    Removed unused helpers

--- config/helpers.js
+++ config/helpers.js
@@ -1,11 +1,4 @@
-//Paths
-const path = require('path');
-const _root = path.resolve(__dirname, '..');
-exports.root = (args) => path.join.apply(path, [_root].concat(Array.prototype.slice.call(arguments, 0)));
-
-
 //Internationalization
 const RIGHT_TO_LEFT_LANGS = ['ar','dv','he','ku','fa','ur']
 exports.RIGHT_TO_LEFT_LANGS = RIGHT_TO_LEFT_LANGS
 exports.IsRightToLeft = (lang) => RIGHT_TO_LEFT_LANGS.includes(lang);
-exports.root = root;
